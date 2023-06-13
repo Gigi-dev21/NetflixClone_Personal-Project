260c29b8549af6fa0c735bf2723fd46f362aa121
embet-mengistu <131568421+embet-mengistu@users.noreply.github.com>
2023-06-12 22:04:13 -0400

    created components

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
+import Header from "./Components/header/Header";
+import Nav from "./Components/nav/Nav";
+import Row from "./Components/rows/Row";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Nav />
+      <Header />
+      <Row />
+    </div>
+  );
 }
 
 export default App;
---
+++ netflix/src/Components/rows/Row.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./row.css";
+
+function Row() {
+  return <div>Row</div>;
+}
+
+export default Row;
